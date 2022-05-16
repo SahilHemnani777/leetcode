bebd3f63fc156b9324e170f5f5df6932f962103a
Sahil Hemnani <67259489+SahilHemnani777@users.noreply.github.com>
2022-05-16 17:38:40 +0530

    Time: 33 ms (34.85%), Space: 59.4 MB (50.94%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -0,0 +1,51 @@
+class Solution {
+        public int shortestPathBinaryMatrix(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return -1;
+        }
+
+        int ans = 0;
+
+        int row = grid.length;
+        int col = grid[0].length;
+
+        if (grid[0][0] == 1 || grid[row - 1][col - 1] == 1) {
+            return -1;
+        }
+
+        int[][] dirs = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+
+        boolean[][] visited = new boolean[row][col];
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0, 0});
+        visited[0][0] = true;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            ans++;
+
+            for (int i = 0; i < size; i++) {
+                int[] curPos = queue.poll();
+
+                if (curPos[0] == row - 1 && curPos[1] == col - 1) {
+                    return ans;
+                }
+
+                for (int[] dir : dirs) {
+                    int nextX = curPos[0] + dir[0];
+                    int nextY = curPos[1] + dir[1];
+
+                    if (nextX < 0 || nextX >= row || nextY < 0 || nextY >= col || visited[nextX][nextY] || grid[nextX][nextY] == 1) {
+                        continue;
+                    }
+
+                    visited[nextX][nextY] = true;
+                    queue.offer(new int[]{nextX, nextY});
+                }
+            }
+        }
+
+        return -1;
+    }
+}
